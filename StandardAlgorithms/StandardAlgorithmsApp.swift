66d7f469c4eb4d6eacce8d652197770515d2a66f
Areeb <Iqba.a@etoncollege.org.uk>
2024-11-05 10:06:14 +0000

    Initial Commit

---
+++ StandardAlgorithms/StandardAlgorithmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandardAlgorithmsApp.swift
+//  StandardAlgorithms
+//
+//  Created by Iqbal, Areeb (AMM) on 05/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct StandardAlgorithmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
