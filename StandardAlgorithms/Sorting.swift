7758960d9fd9dc85bb68c9dac8aa4e9fc0f890c8
Areeb <Iqba.a@etoncollege.org.uk>
2024-11-05 11:19:30 +0000

    initial TDD and bubble sort that is pendng

---
+++ StandardAlgorithms/Sorting.swift
@@ -0,0 +1,22 @@
+//
+//  Sorting.swift
+//  StandardAlgorithms
+//
+//  Created by Iqbal, Areeb (AMM) on 05/11/2024.
+//
+
+import Foundation
+
+class Sorting {
+    func bubbleSort(_ numbers: [Int]) -> [Int] {
+        var count = 0
+        var store = 0
+        var numbers = [12,0,8,4,1]
+        var sorted : [Int] = []
+        for numbers i in  numbers.count & i>0  {
+            
+            
+            
+        }
+    }
+}
