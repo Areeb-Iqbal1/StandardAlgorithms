21065cf272a79825911f96e5f5624f174580cd13
Areeb <Iqba.a@etoncollege.org.uk>
2024-11-07 13:15:08 +0000

    ..

---
+++ StandardAlgorithmsTests/Searching Test.swift
@@ -0,0 +1,20 @@
+//
+//  Searching Test.swift
+//  StandardAlgorithmsTests
+//
+//  Created by Iqbal, Areeb (AMM) on 07/11/2024.
+//
+
+import XCTest
+
+final class Searching_Test: XCTestCase {
+    func TestBinarySearchingWithVariousIntegersToReturnArrayOfSortedIntegers() {
+        var search() = []
+        let Search = Search ()
+        let SearchCases = [(numbers: [4,1,7,9,3] , expected: [1,3,4,7,9,]) ,
+                           (numbers: [4,14,9,0,1], expected: [0,1,4,9,14]),
+                           (numbers: [], expected: []),
+                           (numbers: [1,2,3], expected: [1,2,3]),
+                           (numbers: [1,1,1],expected: [1,1,1])]
+    }
+}
